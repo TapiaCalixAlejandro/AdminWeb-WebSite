31d5d2eaec4c93042ee98ce72531e17a2aff47e4
TapiaCalixAlejandro <tapiacalix6@gmail.com>
2022-02-22 23:30:55 -0600

    Modelo productos

---
+++ database/migrations/2022_02_23_050032_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->double('price');
+            $table->double('shipping')->nullable();
+            $table->integer('stock');
+            $table->string('description');
+            $table->string('image');
+            $table->boolean('status')->default(true);
+            $table->integer('user_id');
+            $table->foreignId('subcategory_id')->constrained('subcategories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
