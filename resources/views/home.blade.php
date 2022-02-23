dd916702232bd8d24981027b3d60ae2f805ea528
TapiaCalixAlejandro <tapiacalix6@gmail.com>
2022-02-22 22:45:32 -0600

    Implementacion de plantilla

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,10 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row-auto d-flex justify-content-center">
+        <div class="col-auto">
+            <div class="card d-flex justify-content-center">
+                <img src="/img/home.png" alt="" height="450" width="450">
             </div>
         </div>
     </div>
